9220f028dda3abd7ce130a62ec67bbb3fa716e01
Adama SAMBE <a.sambe@3sgroupsn.com>
2021-02-15 11:37:43 +0000

    Bismillah

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ Filter every data objects you have.
 Then
 
 ```
-import { filter-pkg } from 'filter-pkg';
-
+import { filterObj } from 'filter-pkg';
+const arr = [1, 23, 56, 2, 9, 10, 7];
+let test = filterObj(arr, 6, {op: 'inf'});
+console.log(arr, test);
 ```
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function filterObj(arr, obj, options) {
+    let filtred = null;
     if(options.length != 0) {
         if(options.op == 'eq') {
-            let filtred = arr.filter(a => a == obj);
+            filtred = arr.filter(a => a == obj);
         } else if(options.op == 'sup') {
-            let filtred = arr.filter(a => a > obj);
+            filtred = arr.filter(a => a > obj);
         } else {
-            let filtred = arr.filter(a => a < obj);
+            filtred = arr.filter(a => a < obj);
         }
     }
     return filtred ? filtred : {msg: 'Undefined options', data: arr};
